4b8a85d37eb4a7368ad50a9065a2f19bbcdebfad
Aderibigbe Ayomide <77296707+UNIWYLIMP@users.noreply.github.com>
2024-09-25 04:26:26 +0100

    Update settings.py

--- tgnc/settings.py
+++ tgnc/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4#(4-r&cn5-jv4orr$$c&#brl)dfg*vuo$1---8@w8vvs(v)4-'
+SECRET_KEY = 'caught-yah!!!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
